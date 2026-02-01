665ecad877e1d25b52a80f6ca6567b6f86dccece
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2026-02-01 13:36:15 -0300

    feat(user): upate User model with document fields

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dom\DocumentType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('document')->unique();
+            $table->enum('document_type', ['CPF', 'CNPJ']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

efff3bd63a07aebf8e9b3adfb3af21b743d592de
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2026-02-02 17:13:55 -0300

    feat(transfer): implement JsonSerializable in TransferOutputDTO for JSON representation of transfer data

--- app/Application/Transfer/DTOs/TransferOutputDTO.php
+++ app/Application/Transfer/DTOs/TransferOutputDTO.php
@@ -5,10 +5,26 @@ declare(strict_types=1);
 namespace App\Application\Transfer\DTOs;
 
 use App\Domain\Transfer\Entity\Transfer;
+use JsonSerializable;
 
-class TransferOutputDTO
+class TransferOutputDTO implements JsonSerializable
 {
     public function __construct(
         public Transfer $transfer,
     ) {}
+
+    /**
+     * @return array<string, string|null>
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->transfer->id,
+            'status' => $this->transfer->status->value,
+            'value' => $this->transfer->amount->toDecimal(),
+            'payer' => $this->transfer->payer->user->id,
+            'payee' => $this->transfer->payee->user->id,
+            'created_at' => $this->transfer->createdAt->format(DATE_ATOM),
+        ];
+    }
 }
